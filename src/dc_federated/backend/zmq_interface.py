e503f3b2ffae7291a01ba9a9f96eee4c0f10d960
Luke Ellison <luke.ellison@digicatapult.org.uk>
2022-01-25 17:48:26 +0000

    tested_zmq_interface

--- src/dc_federated/backend/zmq_interface.py
+++ src/dc_federated/backend/zmq_interface.py
@@ -18,7 +18,7 @@ class ZMQInterfaceModel():
         return_global_model_callback,
         is_global_model_most_recent,
         receive_worker_update_callback,
-        server_subprocess_args
+        server_subprocess_args,
 
     ) -> None:
         self.socket = socket
@@ -38,10 +38,10 @@ class ZMQInterfaceModel():
             self.socket.send_pyobj(self.server_subprocess_args)
         # Federated Learning API
         elif message[0] == b'register_worker':
-            self.register_worker_callback(message[1])
+            self.register_worker_callback(message[1].decode('utf-8'))
             self.socket.send(b'1')
         elif message[0] == b'unregister_worker':
-            self.unregister_worker_callback(message[1])
+            self.unregister_worker_callback(message[1].decode('utf-8'))
             self.socket.send(b'1')
         elif message[0] == b'return_global_model':
             global_model = self.return_global_model_callback()
@@ -50,42 +50,48 @@ class ZMQInterfaceModel():
             most_recent = self.is_global_model_most_recent(int(message[1].decode('utf-8')))
             self.socket.send_pyobj(most_recent)
         elif message[0] == b'receive_worker_update':
-            status = self.receive_worker_update_callback(message[1], message[2])
+            status = self.receive_worker_update_callback(message[1].decode('utf-8'), message[2])
             self.socket.send_string(status)
         else:
             logger.error(f'ZQM messaging interface received unrecognised message type: "{message[0]}"')
 
-class ZQMInterfaceServer():
+class ZMQInterfaceServer():
+    def __init__(self, port) -> None:
+        self.port = port
 
     def server_args_request_send(self):
-        self._send([b'server_args_request'])
-        return self.socket.recv_pyobj()
+        socket = self._send([b'server_args_request'])
+        return socket.recv_pyobj()
 
     def register_worker_send(self, worker_id):
-        self._send([b'register_worker', worker_id.encode('utf-8')])
-        return self.socket.recv()
+        socket = self._send([b'register_worker', worker_id.encode('utf-8')])
+        return socket.recv()
 
     def unregister_worker_send(self, worker_id):
-        self._send([b'unregister_worker', worker_id.encode('utf-8')])
-        return self.socket.recv()
+        socket = self._send([b'unregister_worker', worker_id.encode('utf-8')])
+        return socket.recv()
 
     def return_global_model_send(self):
-        self._send([b'return_global_model'])
-        return self.socket.recv_pyobj()
+        socket = self._send([b'return_global_model'])
+        return socket.recv_pyobj()
     
     def is_global_model_most_recent_send(self, model_version):
-        self._send([b'is_global_model_most_recent', str(model_version).encode('utf-8')])
-        return self.socket.recv_pyobj()
+        socket = self._send([b'is_global_model_most_recent', str(model_version).encode('utf-8')])
+        return socket.recv_pyobj()
 
     def receive_worker_update_send(self, worker_id, model_update):
-        self._send([b'receive_worker_update', worker_id.encode('utf-8'), model_update])
-        return self.socket.recv_string()
+        socket = self._send([b'receive_worker_update', worker_id.encode('utf-8'), model_update])
+        return socket.recv_string()
+
+    def _new_socket(self):
+        context = zmq.Context()
+        socket = context.socket(zmq.REQ)
+        socket.connect(f"tcp://localhost:{self.port}")
+        return socket
 
     def _send(self, args):
-        context = zmq.Context()
-        self.socket = context.socket(zmq.REQ)
-        self.socket.connect(f"tcp://localhost:5555")
-
+        socket = self._new_socket()
         logger.debug(f'Sending zmq message: {args}')
-        self.socket.send_multipart(args)
+        socket.send_multipart(args)
         logger.debug(f'Message sent.')
+        return socket
---
+++ tests/test_zmq_interface.py
@@ -0,0 +1,105 @@
+
+from dc_federated.backend.zmq_interface import ZMQInterfaceModel, ZMQInterfaceServer
+import zmq
+from unittest.mock import Mock, patch
+import threading
+import time
+import pytest
+
+register_worker_callback = Mock()
+unregister_worker_callback = Mock()
+GLOBAL_MODEL = {'test':'test_val'}
+return_global_model_callback = Mock(return_value=GLOBAL_MODEL)
+IS_MOST_RECENT = True
+is_global_model_most_recent = Mock(return_value=IS_MOST_RECENT)
+WORKER_UPDATE='test_string'
+receive_worker_update_callback = Mock(return_value=WORKER_UPDATE)
+server_subprocess_args = ['test', 'subprocess', 'args', 1, True, None]
+port = 5555
+
+def mock_recv(context, socket, fn_name, close=True):
+    original_recv = getattr(socket, fn_name)
+    def recv_py(flags=0):
+        poller = zmq.Poller()
+        poller.register(socket, zmq.POLLIN)
+        while True:
+            evts = poller.poll(500)
+            if len(evts) > 0:
+                result = original_recv(flags=flags)
+                if close:
+                    socket.close()
+                    context.term()
+                return result
+            time.sleep(0.5)
+    
+    setattr(socket, fn_name, Mock(side_effect=recv_py))
+
+def mock_new_socket():
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect(f"tcp://localhost:{port}")
+
+    mock_recv(context, socket, 'recv_pyobj')
+    mock_recv(context, socket, 'recv')
+    mock_recv(context, socket, 'recv_string')
+    return socket
+
+@pytest.fixture(autouse=True)
+def run_model_interface():
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind(f"tcp://*:{port}")
+
+    mock_recv(context, socket, 'recv_multipart', close=False)
+
+    zmqM = ZMQInterfaceModel(
+        socket=socket,
+        register_worker_callback=register_worker_callback,
+        unregister_worker_callback=unregister_worker_callback,
+        return_global_model_callback=return_global_model_callback,
+        is_global_model_most_recent=is_global_model_most_recent,
+        receive_worker_update_callback=receive_worker_update_callback,
+        server_subprocess_args=server_subprocess_args,
+    )
+    thread = threading.Thread(target=zmqM.receive, daemon=True)
+    thread.start()
+    yield thread
+    socket.close()
+    context.term()
+
+zmqS = ZMQInterfaceServer(port=port)
+
+@patch.object(zmqS, '_new_socket', mock_new_socket)
+def test_server_args_request():
+    result = zmqS.server_args_request_send()
+    assert result == server_subprocess_args
+
+@patch.object(zmqS, '_new_socket', mock_new_socket)
+def test_register_worker():
+    result = zmqS.register_worker_send('test123')
+    assert result == b'1'
+    register_worker_callback.assert_called_once_with('test123')
+
+@patch.object(zmqS, '_new_socket', mock_new_socket)
+def test_unregister_worker():
+    result = zmqS.unregister_worker_send('test123')
+    assert result == b'1'
+    unregister_worker_callback.assert_called_once_with('test123')
+
+@patch.object(zmqS, '_new_socket', mock_new_socket)
+def test_return_global_model():
+    result = zmqS.return_global_model_send()
+    assert result == GLOBAL_MODEL
+    return_global_model_callback.assert_called_once_with()
+
+@patch.object(zmqS, '_new_socket', mock_new_socket)
+def test_is_global_model_most_recent():
+    result = zmqS.is_global_model_most_recent_send(123)
+    assert result == IS_MOST_RECENT
+    is_global_model_most_recent.assert_called_once_with(123)
+    
+@patch.object(zmqS, '_new_socket', mock_new_socket)
+def test_receive_worker_update():
+    result = zmqS.receive_worker_update_send('test123', b'model_update')
+    assert result == WORKER_UPDATE
+    receive_worker_update_callback.assert_called_once_with('test123', b'model_update')
